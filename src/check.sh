6b86bced3e083fbd18c7ddb6aed30c6e941381bd
RRRRRm <isxiongzj@gmail.com>
2024-03-12 23:27:22 +0000

    Update check.sh

--- src/check.sh
+++ src/check.sh
@@ -19,6 +19,9 @@ for ddns_record in "${ddns_records[@]}"; do
     export HAS_IPV4=1
   elif [[ $record_type == "AAAA" ]]; then
     export HAS_IPV6=1
+  elif [[ $record_type == "HTTPS" ]]; then
+    export HAS_IPV4=1
+    export HAS_IPV6=1
   else
     echo "Unknown record type $record_type!"
     exit 1
